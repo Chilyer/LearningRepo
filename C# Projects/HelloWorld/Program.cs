9f6c4e549b3edaf3f0b44a0bf24b2faa6239e10a
Christopher Hillyer <hillyerc@dupage.edu>
2023-04-26 13:20:47 -0400

    Hello World Example Update Commit

--- C# Projects/HelloWorld/Program.cs
+++ C# Projects/HelloWorld/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is this new place?");
+            Console.WriteLine("Oh and who might you be?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Why, hello there General " + name + "!");
+
+        }
+    }
+}
